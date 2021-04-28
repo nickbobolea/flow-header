512b0ec4ea8b29fcd1b5b0e3ba2126ae814900f8
Nick Bobolea <alinbobolea@gmail.com>
2021-04-28 15:23:14 -0400

    Added Results

--- symmetric-velocity-temperature/clean.sh
+++ symmetric-velocity-temperature/clean.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd ${0%/*} || exit 1    # Run from this directory
 
-# Remove "0" folder (ICs and BCs)
-rm -rf 0 > /dev/null 2>&1
-
 # Clean "constant" folder
 # Remove blockMesh files
 rm -rf constant/polyMesh > /dev/null 2>&1
@@ -15,7 +12,7 @@ rm -rf constant/extendedFeatureEdgeMesh > /dev/null 2>&1
 # Remove partition folders
 rm -rf processor* > /dev/null 2>&1
 # Remove solver folders
-rm -rf 0 0.[0-9]* [1-9]*
+rm -rf 0 0.[0-9]* [1-9]* > /dev/null 2>&1
 # Remove postProcessing
 rm -rf postProcessing > /dev/null 2>&1
 # Remove log files
