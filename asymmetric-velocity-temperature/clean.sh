1ae6ff6e118a09fd11b987fe3704d99715fe6004
Nick Bobolea <alinbobolea@gmail.com>
2021-04-30 06:41:41 -0400

    Added execution scripts and updated controlDict for the asymmetric case

---
+++ asymmetric-velocity-temperature/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd ${0%/*} || exit 1    # Run from this directory
+
+# Clean "constant" folder
+# Remove blockMesh files
+rm -rf constant/polyMesh > /dev/null 2>&1
+# Remove edge extraction files
+rm -f constant/triSurface/*.eMesh > /dev/null 2>&1
+rm -rf constant/extendedFeatureEdgeMesh > /dev/null 2>&1
+
+# Remove mesh / solver execution folders
+# Remove partition folders
+rm -rf processor* > /dev/null 2>&1
+# Remove solver folders
+rm -rf 0 0.[0-9]* [1-9]* > /dev/null 2>&1
+# Remove postProcessing
+rm -rf postProcessing > /dev/null 2>&1
+# Remove log files
+rm -f log.* > /dev/null 2>&1
+
+#------------------------------------------------------------------------------
---
+++ asymmetric-velocity-temperature/solve.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+cd ${0%/*} || exit 1    # Run from this directory
+
+# Copy 0 boundary conditions
+cp -r 0.orig 0
+
+# Execute solver partition
+start_time="$(date -u +%s.%N)"
+decomposePar | tee log.09.decomposeParSolver
+end_time="$(date -u +%s.%N)"
+elapsed="$(bc <<<"$end_time-$start_time")"
+echo "Execute solver partition: $elapsed seconds" > log.solve
+
+# Execute solver
+start_time="$(date -u +%s.%N)"
+# Parallel (solver partition required)
+mpirun -np 8 buoyantPimpleFoam -parallel | tee log.10.buoyantPimpleFoam
+end_time="$(date -u +%s.%N)"
+elapsed="$(bc <<<"$end_time-$start_time")"
+echo "Execute solver: $elapsed seconds" >> log.solve
+
+# Optional - Solver monitoring
+# foamMonitor -l postProcessing/residuals/0/*.dat
+# foamMonitor postProcessing/outlet_temperature/0/*.dat
+# foamMonitor postProcessing/inlet1_massflow/0/*.dat
+# foamMonitor postProcessing/inlet2_massflow/0/*.dat
+# foamMonitor postProcessing/outlet_massflow/0/*.dat
+# foamMonitor postProcessing/pressure_drop/0/*.dat
+
+# Reconstruct solver solution
+start_time="$(date -u +%s.%N)"
+reconstructPar | tee log.11.reconstructPar
+# reconstructPar -fields '(p p_rgh T U k epsilon phi yPlus)' | tee log.11.reconstructPar
+end_time="$(date -u +%s.%N)"
+elapsed="$(bc <<<"$end_time-$start_time")"
+echo "Reconstruct solver solution: $elapsed seconds" >> log.solve
+
+# Remove solver partition folders
+rm -rf processor* > /dev/null 2>&1
+
+# Optional - Post-process results
+# buoyantPimpleFoam -postProcess -latestTime -dict system/controlDict | tee log.13.postProcess
+# mpirun -np 8 buoyantPimpleFoam -parallel -postProcess -latestTime -dict system/controlDict | tee log.13.postProcess
+
+#------------------------------------------------------------------------------
